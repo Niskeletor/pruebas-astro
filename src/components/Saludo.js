31945e4dd3a17f8608419a35dc6e9843e5bb0ad2
Niskeletor <niskeletor@gmail.com>
2024-01-29 18:17:40 +0100

    proyecto inicial

---
+++ src/components/Saludo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+export function Saludo() {
+
+    return (
+        <div>
+            <h1>Hola Mundo</h1>
+            <p>Esto es un parrafo</p>
+            <button>Click</button>
+        </div>
+    )
+
+}
